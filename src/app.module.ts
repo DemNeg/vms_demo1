1fd95c5d3f41b78b13ebb2b6ca935f66f49c8d4b
DemNeg <basingoudembele@gmail.com>
2022-04-21 16:15:48 +0200

    modules and defaults folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnginModule } from './engin/engin.module';
+import { ProtagonisteModule } from './protagoniste/protagoniste.module';
+import { EtatModule } from './etat/etat.module';
+import { DocModule } from './doc/doc.module';
 
 @Module({
-  imports: [],
+  imports: [EnginModule, ProtagonisteModule, EtatModule, DocModule],
   controllers: [AppController],
   providers: [AppService],
 })
